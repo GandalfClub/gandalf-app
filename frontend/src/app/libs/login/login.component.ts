09051f43bc50f4b21a30cbb58b2132a14efc7f33
Dzmitry <angrybrain@protonmail.com>
2020-07-22 19:27:41 +0300

    fix: test login page

--- frontend/src/app/libs/login/login.component.ts
+++ frontend/src/app/libs/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { Observable, from, Subscription } from 'rxjs';
 import { UserCredentials } from '../auth/models/userCredentials';
 import { AuthFacadeService } from '../auth/store/auth/auth.facade';
 
@@ -10,14 +10,21 @@ import { AuthFacadeService } from '../auth/store/auth/auth.facade';
 export class LoginComponent implements OnInit {
 
 	public credentials: UserCredentials = new UserCredentials();
-	public errorMessage: Observable<string> | null;
-	public isAuthenticated: Observable<boolean>;
+	public errorMessageSub: Subscription;
+	public isAuthenticatedSub: Subscription;
+	public errorMessage: string;
+	public isAuthenticated: boolean;
 
 	constructor(private authFacadeService: AuthFacadeService) { }
 
 	public ngOnInit(): void {
-		this.errorMessage = this.authFacadeService.errorMessage$;
-		this.isAuthenticated = this.authFacadeService.isAuthenticated$;
+		this.errorMessageSub = this.authFacadeService.errorMessage$.subscribe(
+			(error:string) => this.errorMessage = error,
+		);
+		this.isAuthenticatedSub = this.authFacadeService.isAuthenticated$.subscribe(
+			(status:boolean) => this.isAuthenticated = status,
+		);
+		console.log(this.errorMessage, this.isAuthenticated);
 	}
 
 	public onSubmit(): void {
