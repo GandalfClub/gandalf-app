cb5cf78d4a1a704bb43c7bae26eb41c3cbf10ffa
Aliaksei <Palianski>
2020-08-04 22:55:44 +0300

    refactor(gandalf/frontend): minor refactor

--- frontend/src/app/libs/auth-pages/sign-in/sign-in.component.spec.ts
+++ frontend/src/app/libs/auth-pages/sign-in/sign-in.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
+import { of } from 'rxjs';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthFacadeService } from '../../auth/store/auth/auth.facade';
+import { FormBuilder } from '@angular/forms';
 
 describe('SignInComponent', () => {
+	const user: any = {};
 	let component: SignInComponent;
 	let fixture: ComponentFixture<SignInComponent>;
+	const mockAuthFacadeService: any = {
+		get user$(): any {
+			return of(user);
+		},
+	};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [SignInComponent],
+			imports: [EffectsModule.forRoot([]), StoreModule.forRoot({}), RouterTestingModule],
+			providers: [{ provide: AuthFacadeService, useValue: mockAuthFacadeService }, { provide: FormBuilder }],
 		}).compileComponents();
 	}));
 
--- frontend/src/app/libs/auth-pages/sign-up/sign-up.component.spec.ts
+++ frontend/src/app/libs/auth-pages/sign-up/sign-up.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpComponent } from './sign-up.component';
+import { AuthFacadeService } from '../../auth/store/auth/auth.facade';
+import { EntityWrapper } from '../../auth/models/entity-wraper';
+import { of, Observable } from 'rxjs';
+import { User } from '../../auth/models/user';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder } from '@angular/forms';
 
 describe('SignUpComponent', () => {
+	const user: any = {};
 	let component: SignUpComponent;
 	let fixture: ComponentFixture<SignUpComponent>;
+	const mockAuthFacadeService: any = {
+		get user$(): any {
+			return of(user);
+		},
+	};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [SignUpComponent],
+			imports: [EffectsModule.forRoot([]), StoreModule.forRoot({}), RouterTestingModule],
+			providers: [{ provide: AuthFacadeService, useValue: mockAuthFacadeService }, { provide: FormBuilder }],
 		}).compileComponents();
 	}));
 
