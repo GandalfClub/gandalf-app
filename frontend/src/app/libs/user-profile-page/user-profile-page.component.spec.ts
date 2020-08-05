4d5e84319aebc5b0c24335257f177b03ef042b8a
Dzmitri <dzmitrilituyev@gmail.com>
2020-08-05 11:57:08 +0300

    fix(gandalf/frontend): changes store added

--- frontend/src/app/libs/user-profile-page/user-profile-page.component.spec.ts
+++ frontend/src/app/libs/user-profile-page/user-profile-page.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AuthFacadeService } from '../auth/store/auth/auth.facade';
 import { UserProfilePageComponent } from './user-profile-page.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
+import { UserProfileService } from './service/user-profile-service';
 
 describe('UserConfigComponent', () => {
+	const user: any = {};
 	let component: UserProfilePageComponent;
 	let fixture: ComponentFixture<UserProfilePageComponent>;
-
+	const mockAuthFacadeService: any = {
+		get user$(): any {
+			return of(user);
+		},
+	  get signInByGithub(): any {return {}; },
+	};
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [UserProfilePageComponent],
+			imports: [EffectsModule.forRoot([]), StoreModule.forRoot({}), RouterTestingModule],
+			providers: [
+				{ provide: AuthFacadeService, useValue: mockAuthFacadeService },
+				{ provide: FormBuilder },
+				{ provide: UserProfileService },
+			],
 		}).compileComponents();
 	}));
 
