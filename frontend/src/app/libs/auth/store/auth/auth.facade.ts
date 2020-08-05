dbf2225d359752cf9fad4a00b934029d212e58d8
rat6666 <54438368+rat6666@users.noreply.github.com>
2020-08-05 12:32:39 +0300

    fix(gandalf/frontend): auth store fix (#17)
    * fix(gandalf/frontend): auth store fix
    
    * fix(gandalf/frontend): login.ts fixed for siccess test
    
    Co-authored-by: Aliaksei <Palianski>

--- frontend/src/app/libs/auth/store/auth/auth.facade.ts
+++ frontend/src/app/libs/auth/store/auth/auth.facade.ts
@@ -3,17 +3,15 @@ import { Store, select } from '@ngrx/store';
 import { SignIn, SignInByGithub, SignUp } from './auth.actions';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user';
-import { selectUser, selectStatus } from './auth.selectors';
+import { selectUser } from './auth.selectors';
 import { AuthState } from '../../models/auth-state';
+import { EntityWrapper } from '../../models/entity-wraper';
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class AuthFacadeService {
-
-	constructor(
-		private store: Store<AuthState>
-	) { }
+	constructor(private store: Store<AuthState>) {}
 
 	public signIn(email: string, password: string): void {
 		this.store.dispatch(new SignIn({ email, password }));
@@ -27,11 +25,7 @@ export class AuthFacadeService {
 		this.store.dispatch(new SignUp({ email, password }));
 	}
 
-	get user$(): Observable<User> {
+	get user$(): Observable<EntityWrapper<User>> {
 		return this.store.pipe(select(selectUser));
-	}
-
-	get status$(): Observable<string> {
-		return this.store.pipe(select(selectStatus));
 	}
 }
