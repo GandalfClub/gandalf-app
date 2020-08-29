62c9d5312cc40f7b79fee4900746e537036693c6
rat6666 <54438368+rat6666@users.noreply.github.com>
2020-08-29 09:41:27 +0300

    fix(gandalf/frontend): code jar test fixed

--- frontend/src/app/libs/code-editors/codejar-editor/codejar.component.spec.ts
+++ frontend/src/app/libs/code-editors/codejar-editor/codejar.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CodejarComponent } from './codejar.component';
 import { SimpleChanges, SimpleChange } from '@angular/core';
 import * as Prism from 'prismjs';
-import {Language} from '../shared/enum/languages.enum';
+import { Language } from '../shared/enum/languages.enum';
 
 describe('CodejarComponent', () => {
 	let component: CodejarComponent;
@@ -60,9 +60,7 @@ describe('CodejarComponent', () => {
 		});
 
 		it('editor must be updated with code', () => {
-			expect(component.editor.updateCode).toHaveBeenCalledWith(
-				component.code
-			);
+			expect(component.editor.updateCode).toHaveBeenCalledWith(component.code);
 		});
 	});
 
@@ -70,11 +68,7 @@ describe('CodejarComponent', () => {
 		beforeEach(() => {
 			changes = {};
 			component.editor = mockCodeJar;
-			changes.language = new SimpleChange(
-				previousValue,
-				currentValue,
-				true
-			);
+			changes.language = new SimpleChange(previousValue, currentValue, true);
 			component.language = Language.JAVASCRIPT;
 			component.ngOnChanges(changes);
 		});
