6a1c84b2e76b9650bc763f218360fa85285fcba1
rat6666 <54438368+rat6666@users.noreply.github.com>
2020-08-29 09:53:21 +0300

    fix(gandalf/frontend): reset

--- frontend/src/app/libs/code-editors/codejar-editor/codejar.component.spec.ts
+++ frontend/src/app/libs/code-editors/codejar-editor/codejar.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CodejarComponent } from './codejar.component';
 import { SimpleChanges, SimpleChange } from '@angular/core';
 import * as Prism from 'prismjs';
-import { Language } from '../shared/enum/languages.enum';
+import {Language} from '../shared/enum/languages.enum';
 
 describe('CodejarComponent', () => {
 	let component: CodejarComponent;
@@ -18,10 +18,13 @@ describe('CodejarComponent', () => {
 		toString: jasmine.createSpy('toString').and.returnValue('test'),
 	} as any;
 
-	beforeEach(() => {
+	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [CodejarComponent],
 		}).compileComponents();
+	}));
+
+	beforeEach(() => {
 		spyOn(Prism, 'highlight');
 		fixture = TestBed.createComponent(CodejarComponent);
 		component = fixture.componentInstance;
@@ -57,7 +60,9 @@ describe('CodejarComponent', () => {
 		});
 
 		it('editor must be updated with code', () => {
-			expect(component.editor.updateCode).toHaveBeenCalledWith(component.code);
+			expect(component.editor.updateCode).toHaveBeenCalledWith(
+				component.code
+			);
 		});
 	});
 
@@ -65,7 +70,11 @@ describe('CodejarComponent', () => {
 		beforeEach(() => {
 			changes = {};
 			component.editor = mockCodeJar;
-			changes.language = new SimpleChange(previousValue, currentValue, true);
+			changes.language = new SimpleChange(
+				previousValue,
+				currentValue,
+				true
+			);
 			component.language = Language.JAVASCRIPT;
 			component.ngOnChanges(changes);
 		});
