dab2d4b467d88c9a975822edce6084dd0ea2c8aa
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-01 22:27:34 +0300

    feat: add event-tile template

--- frontend/src/app/libs/common-components/common-components.module.ts
+++ frontend/src/app/libs/common-components/common-components.module.ts
@@ -5,9 +5,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EventTileComponent } from './components/event-tile/event-tile.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-	declarations: [EventTileComponent],
-	exports: [MatInputModule, MatButtonModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule],
+	exports: [MatInputModule, MatButtonModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, EventTileComponent],
+	imports: [ CommonModule ]
 })
 export class CommonComponentsModule {}
--- frontend/src/app/libs/common-components/components/event-tile/event-tile.component.ts
+++ frontend/src/app/libs/common-components/components/event-tile/event-tile.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-tile',
   templateUrl: './event-tile.component.html',
   styleUrls: ['./event-tile.component.scss']
 })
-export class EventTileComponent implements OnInit {
+export class EventTileComponent {
 
-  constructor() {
-		
-	}
+	@Input()
+	public name: string;
 
-  public ngOnInit(): void {
+	@Input()
+	public startDate: Date;
 
-  }
+	@Input()
+	public endDate: Date;
+
+	@Input()
+	public startTime: Date;
+
+	@Input()
+	public endTime: Date;
+
+	@Input()
+	public menuActions: Map<string, Function>;
+
+	@Input()
+	public isDraft: boolean;
+
+	@Input()
+	public statusList: string[];
+
+  constructor() {}
 }
