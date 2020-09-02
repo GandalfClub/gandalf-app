c446df8138082e3bbbb911e2ea51d17bfb574fe1
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-02 18:11:54 +0300

    feat: add event-tile styles

--- frontend/src/app/libs/common-components/common-components.module.ts
+++ frontend/src/app/libs/common-components/common-components.module.ts
@@ -3,13 +3,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EventTileComponent } from './components/event-tile/event-tile.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
 	declarations: [ EventTileComponent ],
-	exports: [MatInputModule, MatButtonModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, EventTileComponent],
-	imports: [ CommonModule ]
+	exports: [ MatInputModule, MatButtonModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, EventTileComponent ],
+	imports: [ CommonModule, MatIconModule, MatButtonModule, MatTooltipModule]
 })
 export class CommonComponentsModule {}
