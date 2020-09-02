b91a25d450bdddda89d093e7c876c4d88dac943b
rat6666 <54438368+rat6666@users.noreply.github.com>
2020-09-02 09:07:24 +0300

    feat(gandalf/backend): event route added

--- backend/src/components/PublicEvents/index.ts
+++ backend/src/components/PublicEvents/index.ts
@@ -12,3 +12,13 @@ export async function getAllEvents(req: Request, res: Response, next: NextFuncti
 		next(new HttpError(error.message.status, error.message));
 	}
 }
+
+export async function getEvent(req: Request, res: Response, next: NextFunction): Promise<void> {
+	try {
+		const event: IPublicEventsModel = await PublicEventsService.getEvent(req.params.id);
+
+		res.status(200).json(event);
+	} catch (error) {
+		next(new HttpError(error.message.status, error.message));
+	}
+}
--- backend/src/components/PublicEvents/interface.ts
+++ backend/src/components/PublicEvents/interface.ts
@@ -2,4 +2,11 @@ import { IPublicEventsModel } from './model';
 
 export interface IPublicEventsService {
 	getAllEvents(): Promise<IPublicEventsModel[]>;
+
+	/**
+	 * @param {string} code
+	 * @returns {Promise<IUserModel>}
+	 * @memberof IUserService
+	 */
+	getEvent(id: string): Promise<IPublicEventsModel>;
 }
