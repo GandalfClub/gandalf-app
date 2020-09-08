25f3a6aa81bf9593cfc99fb1aaf3acfc9f79df61
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-08 13:22:51 +0300

    test: add event-tile unit tests

--- frontend/src/app/libs/common-components-demo/event-tile-demo/event-tile-demo.component.spec.ts
+++ frontend/src/app/libs/common-components-demo/event-tile-demo/event-tile-demo.component.spec.ts
@@ -1,25 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventTileDemoComponent } from './event-tile-demo.component';
+import { ComponentsDemoGroupComponent } from '../components-demo-group/components-demo-group.component';
 
 describe('EventTileDemoComponent', () => {
-  let component: EventTileDemoComponent;
-  let fixture: ComponentFixture<EventTileDemoComponent>;
+	let component: EventTileDemoComponent;
+	let fixture: ComponentFixture<EventTileDemoComponent>;
+	let htmlElement: HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EventTileDemoComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ EventTileDemoComponent ]
+		})
+		.compileComponents();
+		fixture = TestBed.createComponent(EventTileDemoComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EventTileDemoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		htmlElement = fixture.nativeElement;
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('when @Input gets isDark', () => {
+		beforeEach(() => {
+			component.isDark = true;
+			fixture.detectChanges();
+		});
+
+		it('should define isDark', () => {
+			expect(component.isDark).toBeTrue();
+		});
+	});
+
+	describe('when @Input does not get isDark', () => {
+		it('should be false by default', () => {
+			expect(component.isDark).toBeFalse();
+		});
+	});
 });
