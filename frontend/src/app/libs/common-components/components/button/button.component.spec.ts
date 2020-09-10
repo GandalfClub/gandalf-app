91daafcff7e69b2c5f93df141c585f09ded4db62
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-10 11:47:40 +0300

    test: add unit tests for button common component

--- frontend/src/app/libs/common-components/components/button/button.component.spec.ts
+++ frontend/src/app/libs/common-components/components/button/button.component.spec.ts
@@ -41,7 +41,7 @@ describe('ButtonComponent', () => {
 				fixture.detectChanges();
 			});
 
-			it('should show basic button', () => {
+			it('should show outlined button', () => {
 				expect(htmlElement.querySelector('.button.button--outlined')).toBeTruthy();
 			});
 		});
@@ -52,9 +52,74 @@ describe('ButtonComponent', () => {
 				fixture.detectChanges();
 			});
 
-			it('should show basic button', () => {
+			it('should show flat button', () => {
 				expect(htmlElement.querySelector('.button.button--flat')).toBeTruthy();
 			});
+		});
+	});
+
+	describe('when @Input gets isDark', () => {
+		describe('isDark is truthy', () => {
+			beforeEach(() => {
+				component.isDark = true;
+				fixture.detectChanges();
+			});
+
+			it('should show dark themed button', () => {
+				expect(htmlElement.querySelector('.button.button--dark-theme')).toBeTruthy();
+			});
+		});
+		describe('isDark is falsy', () => {
+			beforeEach(() => {
+				component.isDark = false;
+				fixture.detectChanges();
+			});
+
+			it('should show light themed button', () => {
+				expect(htmlElement.querySelector('.button.button--dark-theme')).toBeNull();
+			});
+		});
+	});
+
+	describe('when @Input gets isDisabled', () => {
+		describe('isDisabled truthy', () => {
+			beforeEach(() => {
+				component.isDisabled = true;
+				fixture.detectChanges();
+			});
+
+			it('should show disabled button', () => {
+				expect(htmlElement.querySelector('.button[disabled]')).toBeTruthy();
+			});
+		});
+
+		describe('isDisabled falsy', () => {
+			beforeEach(() => {
+				component.isDisabled = false;
+				fixture.detectChanges();
+			});
+
+			it('should show enabled button', () => {
+				expect(htmlElement.querySelector('.button[disabled]')).toBeNull();
+			});
+		});
+	});
+
+	describe('when @Input does not get type', () => {
+		it('should show basic button', () => {
+			expect(htmlElement.querySelector('.button.button--basic')).toBeTruthy();
+		});
+	});
+
+	describe('when @Input does not get isDark', () => {
+		it('should show light themed button', () => {
+			expect(htmlElement.querySelector('.button.button--dark-theme')).toBeNull();
+		});
+	});
+
+	describe('when @Input does not get isDisabled', () => {
+		it('should show enabled button', () => {
+			expect(htmlElement.querySelector('.button[disabled="true"]')).toBeNull();
 		});
 	});
 });
