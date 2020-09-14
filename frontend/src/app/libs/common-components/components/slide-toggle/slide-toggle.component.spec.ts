5a7d56adeab0056ef2e58e665ce6074647858bcb
Artur <60847370+Keczale@users.noreply.github.com>
2020-09-14 14:57:42 +0300

    fix: tests fixed - renaimed vriales (#59)
    Co-authored-by: Artur Makaranka <artur_makaranka@epam.com>

--- frontend/src/app/libs/common-components/components/slide-toggle/slide-toggle.component.spec.ts
+++ frontend/src/app/libs/common-components/components/slide-toggle/slide-toggle.component.spec.ts
@@ -4,6 +4,7 @@ import { SlideToggleComponent } from './slide-toggle.component';
 import { CommonComponentsModule } from '../../common-components.module';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { ComponentTheme } from '../../shared/component-theme.enum';
 
 describe('SlideToggleComponent', () => {
   let component: SlideToggleComponent;
@@ -13,29 +14,29 @@ describe('SlideToggleComponent', () => {
   let toggleDebug: DebugElement;
 
   beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [ SlideToggleComponent ],
-			imports: [CommonComponentsModule]
-		})
+	TestBed.configureTestingModule({
+		declarations: [ SlideToggleComponent ],
+		imports: [CommonComponentsModule]
+	})
   .compileComponents();
   }));
 
   beforeEach(() => {
-		fixture = TestBed.createComponent(SlideToggleComponent);
-		component = fixture.componentInstance;
-		htmlElement = fixture.nativeElement;
-		toggleEl = fixture.debugElement.query(By.css('mat-slide-toggle')).nativeElement;
-		component.isDisabled = false;
-		component.isDarkTheme = true;
-		fixture.detectChanges();
+	fixture = TestBed.createComponent(SlideToggleComponent);
+	component = fixture.componentInstance;
+	htmlElement = fixture.nativeElement;
+	toggleEl = fixture.debugElement.query(By.css('mat-slide-toggle')).nativeElement;
+	component.disabled = false;
+	component.theme = ComponentTheme.Dark;
+	fixture.detectChanges();
   });
 
   it('should create', () => {
-		expect(component).toBeTruthy();
+	expect(component).toBeTruthy();
   });
 
   it('should become non disabled when @Input isDisabled===false', () => {
-		expect(toggleEl.attributes['ng-reflect-disabled'].value).toBe('false');
+	expect(toggleEl.attributes['ng-reflect-disabled'].value).toBe('false');
   });
 
   it('should support black theme', () => {
@@ -43,15 +44,15 @@ describe('SlideToggleComponent', () => {
   });
 
   describe('toggle status', () => {
-		beforeEach(() => {
-			toggleDebug = fixture.debugElement.query(By.css('mat-slide-toggle'));
-			toggleDebug.triggerEventHandler('change', null);
-			fixture.detectChanges();
-		});
+	beforeEach(() => {
+		toggleDebug = fixture.debugElement.query(By.css('mat-slide-toggle'));
+		toggleDebug.triggerEventHandler('change', null);
+		fixture.detectChanges();
+	});
 
-		it('should send proper Output', () => {
-			expect(component.isActive).toBe(true);
-		});
+	it('should send proper Output', () => {
+		expect(component.value).toBe(true);
+	});
 
   });
 
