b10ea1a6c983ae7fb9e7fed61246adb7881f2f3a
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-15 14:30:41 +0300

    fix: linter warnings

--- frontend/src/app/libs/common-components-demo/components/scrollbar-demo/scrollbar-demo.component.spec.ts
+++ frontend/src/app/libs/common-components-demo/components/scrollbar-demo/scrollbar-demo.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScrollbarDemoComponent } from './scrollbar-demo.component';
 
 describe('ScrollbarDemoComponent', () => {
-  let component: ScrollbarDemoComponent;
-  let fixture: ComponentFixture<ScrollbarDemoComponent>;
+	let component: ScrollbarDemoComponent;
+	let fixture: ComponentFixture<ScrollbarDemoComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ScrollbarDemoComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ ScrollbarDemoComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ScrollbarDemoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ScrollbarDemoComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- frontend/src/app/libs/common-components-demo/components/scrollbar-demo/scrollbar-demo.component.ts
+++ frontend/src/app/libs/common-components-demo/components/scrollbar-demo/scrollbar-demo.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-scrollbar-demo',
   templateUrl: './scrollbar-demo.component.html',
   styleUrls: ['./scrollbar-demo.component.scss']
 })
-export class ScrollbarDemoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ScrollbarDemoComponent {}
