4e0d514aa5becb05004920bfa5bbe05f2b661965
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-16 19:03:40 +0300

    feat: add scrollbar directive

---
+++ frontend/src/app/libs/common-components/directives/scrollbar/scrollbar.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { ComponentTheme } from '../../shared/component-theme.enum';
+
+@Directive({
+	selector: '[appScrollbar]'
+})
+export class ScrollbarDirective implements OnInit {
+
+	private scrollbarClassName: string = 'scrollbar';
+	private scrollbarLightClassName: string = 'scrollbar--light-theme';
+	private scrollbarDarkClassName: string = 'scrollbar--dark-theme';
+
+	@Input()
+	public theme: ComponentTheme = ComponentTheme.Light;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+	public ngOnInit(): void {
+		this.renderer.addClass(this.el.nativeElement, this.scrollbarClassName);
+
+		switch (this.theme) {
+			case ComponentTheme.Light:
+				this.renderer.addClass(this.el.nativeElement, this.scrollbarLightClassName);
+				break;
+			case ComponentTheme.Dark:
+				this.renderer.addClass(this.el.nativeElement, this.scrollbarDarkClassName);
+				break;
+			default:
+				this.renderer.addClass(this.el.nativeElement, this.scrollbarLightClassName);
+		}
+	}
+}
