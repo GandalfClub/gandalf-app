868f65417a1ce5d06a4af3df7a3b494e1b07c32f
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-09-23 10:23:09 +0300

    refactor: convert spaces to tabs

--- frontend/src/app/libs/common-components-demo/components/tabs-demo/tabs-demo.component.spec.ts
+++ frontend/src/app/libs/common-components-demo/components/tabs-demo/tabs-demo.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabsDemoComponent } from './tabs-demo.component';
 
 describe('TabsDemoComponent', () => {
-  let component: TabsDemoComponent;
-  let fixture: ComponentFixture<TabsDemoComponent>;
+	let component: TabsDemoComponent;
+	let fixture: ComponentFixture<TabsDemoComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TabsDemoComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ TabsDemoComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabsDemoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TabsDemoComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
