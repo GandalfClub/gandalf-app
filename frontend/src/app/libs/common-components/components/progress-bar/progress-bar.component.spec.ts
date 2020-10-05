4da8bf53e88b622f2119a47e460fd811dfb327f3
Artur Makaranka <artur_makaranka@epam.com>
2020-10-05 22:53:54 +0300

    feat: added test on input dark theme

--- frontend/src/app/libs/common-components/components/progress-bar/progress-bar.component.spec.ts
+++ frontend/src/app/libs/common-components/components/progress-bar/progress-bar.component.spec.ts
@@ -2,6 +2,7 @@ import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { By } from '@angular/platform-browser';
+import { ComponentTheme } from '../../shared/component-theme.enum';
 
 import { ProgressBarComponent } from './progress-bar.component';
 
@@ -11,6 +12,7 @@ describe('ProgressBarComponent', () => {
 	const progress: number = 40;
 	const percent: number = 100;
 	let progressFill: DebugElement = null;
+	let htmlElement: HTMLElement;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -23,7 +25,12 @@ describe('ProgressBarComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(ProgressBarComponent);
 		component = fixture.componentInstance;
+
+		htmlElement = fixture.nativeElement;
+
 		component.progress = progress;
+		component.theme = ComponentTheme.Dark;
+
 		fixture.detectChanges();
 	});
 
@@ -31,9 +38,17 @@ describe('ProgressBarComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('should render a properly progress status', () => {
-		progressFill = fixture.debugElement.query(By.css('.mat-progress-bar-fill'));
-		expect(progressFill.nativeElement.style.transform).toEqual(`scaleX(${progress / percent})`);
+	describe('when @Input gets progress value', () => {
+		it('should render a properly progress status', () => {
+			progressFill = fixture.debugElement.query(By.css('.mat-progress-bar-fill'));
+			expect(progressFill.nativeElement.style.transform).toEqual(`scaleX(${progress / percent})`);
+		});
+	});
+
+	describe('when theme is Dark', () => {
+		it('should include dark-theme class', () => {
+			expect(htmlElement.querySelector('.progress-bar--dark-theme')).toBeTruthy();
+		});
 	});
 
 });
