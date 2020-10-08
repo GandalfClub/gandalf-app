d7ba43ca3216d5d694cc2b9e5e4936e4d1654167
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-10-08 14:37:59 +0300

    fix: add animation browser moule to app moddule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContainerModule } from './libs/container/container.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { CodejarComponent } from './libs/code-editors/codejar-editor/codejar.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, CodejarComponent],
 	imports: [
-		StoreModule.forRoot({}),
-		EffectsModule.forRoot([]),
-		StoreDevtoolsModule.instrument({
-			maxAge: 25,
-			logOnly: environment.production
-		}),
 		BrowserModule,
 		AppRoutingModule,
-		ContainerModule
+		BrowserAnimationsModule,
+		ContainerModule,
+		StoreModule.forRoot({}),
+		EffectsModule.forRoot([]),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
 	],
 	providers: [],
 	bootstrap: [AppComponent],
