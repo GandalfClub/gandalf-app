c005ed899528a548e09c5074d05a163ca416afb6
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-10-08 14:10:44 +0300

    fix: remove extra imports from app-module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,40 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonComponentsModule } from './libs/common-components/common-components.module';
 import { ContainerModule } from './libs/container/container.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { CodejarComponent } from './libs/code-editors/codejar-editor/codejar.component';
-import { AuthModule } from './libs/auth/auth.module';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-	declarations: [AppComponent, CodejarComponent],
+	declarations: [AppComponent],
 	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		CommonComponentsModule,
-		ContainerModule,
 		StoreModule.forRoot({}),
 		EffectsModule.forRoot([]),
-		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-		AuthModule,
-		LayoutModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatSidenavModule,
-		MatIconModule,
-		MatListModule,
+		StoreDevtoolsModule.instrument({
+			maxAge: 25,
+			logOnly: environment.production
+		}),
+		BrowserModule,
+		AppRoutingModule,
+		ContainerModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
