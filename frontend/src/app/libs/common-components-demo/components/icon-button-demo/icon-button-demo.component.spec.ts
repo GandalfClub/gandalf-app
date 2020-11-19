50420348bbdc6d26b57eea83f69346b6d16b1fc9
Artur Makaranka <artur_makaranka@epam.com>
2020-11-19 13:38:16 +0300

    refactor: specs indentation changed

--- frontend/src/app/libs/common-components-demo/components/icon-button-demo/icon-button-demo.component.spec.ts
+++ frontend/src/app/libs/common-components-demo/components/icon-button-demo/icon-button-demo.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IconButtonDemoComponent } from './icon-button-demo.component';
 
 describe('IconButtonDemoComponent', () => {
-  let component: IconButtonDemoComponent;
-  let fixture: ComponentFixture<IconButtonDemoComponent>;
+	let component: IconButtonDemoComponent;
+	let fixture: ComponentFixture<IconButtonDemoComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IconButtonDemoComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ IconButtonDemoComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IconButtonDemoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(IconButtonDemoComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
