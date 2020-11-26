602ab4f15d91efada7bb566ee1ed58710cf5b85e
Margarita <57685287+margarita-sh@users.noreply.github.com>
2020-11-26 16:16:31 +0300

    feat: redisign choose language (#92)
    * feat: redisign choose language
    
    * feat: changed img from png to svg (#89)
    
    * refactor: formatted scss, moved logic to component

--- frontend/src/app/libs/container/components/localization/localization.component.ts
+++ frontend/src/app/libs/container/components/localization/localization.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ComponentTheme } from 'src/app/libs/common-components/shared/component-theme.enum';
+import { LanguageButton } from './models/localization-buttons.model';
 
 @Component({
 	selector: 'app-localization',
@@ -10,6 +11,19 @@ import { ComponentTheme } from 'src/app/libs/common-components/shared/component-
 export class LocalizationComponent {
 
 	public darkTheme: ComponentTheme = ComponentTheme.Dark;
+	public languageButtons: LanguageButton[] = [
+		{
+			value: 'BUTTON.LANGUAGE-EN',
+			url: 'assets/images/localization/icon-uk-flag.svg',
+			locale: 'en'
+		},
+		{
+			value: 'BUTTON.LANGUAGE-RU',
+			url: 'assets/images/localization/icon-russian-flag.svg',
+			locale: 'ru'
+		}
+	];
 
-	constructor(public translateService: TranslateService) {}
+	constructor(public translateService: TranslateService) { }
+
 }
