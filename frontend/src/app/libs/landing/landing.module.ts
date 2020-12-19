30a2f7c0ad2e2401c7c790542d7f80a189ee4cb7
margarita-sh <margaritka1992@gmail.com>
2020-12-20 02:24:33 +0300

    feat: added img, css (#88)

--- frontend/src/app/libs/landing/landing.module.ts
+++ frontend/src/app/libs/landing/landing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { CommonComponentsModule } from '../common-components/common-components.module';
 
 import { AuthInterceptor } from '../auth/interceptors/auth.interceptor';
@@ -11,11 +11,12 @@ import { PublicEventsListComponent } from './components/events-page/components/p
 import { EventComponent } from './components/event/event.component';
 import { UserEventsListComponent } from './components/events-page/components/user-events-list/user-events-list.component';
 import { EventListComponent } from './components/event-list/event-list.component';
+import { LocalizationModule } from '../container/components/localization/localization.module';
 
 @NgModule({
 	declarations: [PublicEventsListComponent, EventComponent, LandingPageComponent, UserEventsListComponent, EventListComponent],
-	imports: [CommonComponentsModule, CommonModule, EventsStoreModule],
+	imports: [CommonComponentsModule, CommonModule, EventsStoreModule, LocalizationModule],
 	exports: [LandingRoutingModule],
 	providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
 })
-export class LandingModule {}
+export class LandingModule { }
