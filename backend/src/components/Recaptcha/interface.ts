a02e7306d73e06f39f681fbff53ad68ba4ba5991
Tatsiana <Tatsiana_Shynkevich@epam.com>
2021-02-04 17:18:19 +0300

    feat: add recaptcha v3 API

---
+++ backend/src/components/Recaptcha/interface.ts
@@ -0,0 +1,41 @@
+import { IUserModel } from './model';
+
+/**
+ * @export
+ * @interface IUserService
+ */
+export interface IUserService {
+	/**
+	 * @returns {Promise<IUserModel[]>}
+	 * @memberof IUserService
+	 */
+	findAllUsers(): Promise<IUserModel[]>;
+
+	/**
+	 * @param {string} code
+	 * @returns {Promise<IUserModel>}
+	 * @memberof IUserService
+	 */
+	findUser(id: string): Promise<IUserModel>;
+
+	/**
+	 * @param {IUserModel} IUserModel
+	 * @returns {Promise<IUserModel>}
+	 * @memberof IUserService
+	 */
+	createUser(IUserModel: IUserModel): Promise<IUserModel>;
+
+	/**
+	 * @param {IUserModel} IUserModel
+	 * @returns {Promise<IUserModel>}
+	 * @memberof IUserService
+	 */
+	updateUser(IUserModel: IUserModel): Promise<IUserModel>;
+
+	/**
+	 * @param {string} id
+	 * @returns {Promise<IUserModel>}
+	 * @memberof IUserService
+	 */
+	removeUser(id: string): Promise<IUserModel>;
+}
---
+++ backend/src/components/Recaptcha/service.ts
@@ -0,0 +1,126 @@
+import * as Joi from 'joi';
+import { Types } from 'mongoose';
+import { IUserService } from './interface';
+import UserModel, { IUserModel } from './model';
+import UserValidation from './validation';
+
+/**
+ * @export
+ * @implements {IUserModelService}
+ */
+const UserService: IUserService = {
+	/**
+	 * @returns {Promise < IUserModel[] >}
+	 * @memberof UserService
+	 */
+	async findAllUsers(): Promise<IUserModel[]> {
+		try {
+			return await UserModel.find({});
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	},
+
+	/**
+	 * @param {string} id
+	 * @returns {Promise < IUserModel >}
+	 * @memberof UserService
+	 */
+	async findUser(id: string): Promise<IUserModel> {
+		try {
+			const validate: Joi.ValidationResult<{
+				id: string;
+			}> = UserValidation.findUser({
+				id,
+			});
+
+			if (validate.error) {
+				throw new Error(validate.error.message);
+			}
+
+			return await UserModel.findOne({
+				_id: Types.ObjectId(id),
+			});
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	},
+
+	/**
+	 * @param {IUserModel} user
+	 * @returns {Promise < IUserModel >}
+	 * @memberof UserService
+	 */
+	async createUser(body: IUserModel): Promise<IUserModel> {
+		try {
+			const validate: Joi.ValidationResult<IUserModel> = UserValidation.createUser(body);
+
+			if (validate.error) {
+				throw new Error(validate.error.message);
+			}
+
+			const user: IUserModel = await UserModel.create(body);
+
+			return user;
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	},
+
+	/**
+	 * @param {IUserModel} user
+	 * @returns {Promise < IUserModel >}
+	 * @memberof UserService
+	 */
+	async updateUser(body: IUserModel): Promise<IUserModel> {
+		try {
+			const validate: Joi.ValidationResult<IUserModel> = UserValidation.updateUser(body);
+			if (validate.error) {
+				throw new Error(validate.error.message);
+			}
+			const user: IUserModel = await UserModel.findById(body._id);
+			user.firstName = body.firstName;
+			user.secondName = body.secondName;
+			user.mobilePhone = body.secondName;
+			user.email = body.email;
+			user.isAdmin = body.isAdmin;
+			user.claims = body.claims;
+			user.displayName = body.displayName;
+			user.photoUrl = body.photoUrl;
+			await UserModel.findByIdAndUpdate(body._id, body);
+
+			return user;
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	},
+
+	/**
+	 * @param {string} id
+	 * @returns {Promise < IUserModel >}
+	 * @memberof UserService
+	 */
+	async removeUser(id: string): Promise<IUserModel> {
+		try {
+			const validate: Joi.ValidationResult<{
+				id: string;
+			}> = UserValidation.removeUser({
+				id,
+			});
+
+			if (validate.error) {
+				throw new Error(validate.error.message);
+			}
+
+			const user: IUserModel = await UserModel.findOneAndRemove({
+				_id: Types.ObjectId(id),
+			});
+
+			return user;
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	},
+};
+
+export default UserService;
