9b08bf097bd038d8d475d49e80de5c84f1c7f9ed
Maksim Protas <maksim_protas@epam.com>
2021-02-10 18:31:49 +0300

    feat: add tasks creator

--- frontend/src/app/libs/common-components/components/tasks-creator/models/tasks-creator.ts
+++ frontend/src/app/libs/common-components/components/tasks-creator/models/tasks-creator.ts
@@ -1,6 +1,6 @@
 export enum TasksTypes {
-  coding = 'Coding task',
-  single = 'Single answer',
-  multiple = 'Multiple answers',
-  text = 'Text answer',
+  'coding' = 'CODING',
+  'single' = 'SINGLE',
+  'multiple' = 'MULTIPLE',
+  'text' = 'TEXT',
 }
--- frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator.module.ts
+++ frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { TasksCreatorComponent } from './tasks-creator/tasks-creator.component';
 import { CommonComponentsModule } from '../../common-components.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [TasksListComponent, TasksCreatorComponent],
   exports: [TasksListComponent, TasksCreatorComponent],
-  imports: [CommonModule, CommonComponentsModule],
+  imports: [CommonModule, CommonComponentsModule, TranslateModule],
 })
 export class TasksCreatorModule {}
