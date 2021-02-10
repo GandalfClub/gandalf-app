7e4feaeb1dd0d0568c1e3574dfa8cb115bfcc675
Maksim Protas <maksim_protas@epam.com>
2021-02-10 21:32:10 +0300

    feat: add tasks creator

--- frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator.module.ts
+++ frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator.module.ts
@@ -4,10 +4,12 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { TasksCreatorComponent } from './tasks-creator/tasks-creator.component';
 import { CommonComponentsModule } from '../../common-components.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { MonacoEditorComponent } from '../../../code-editors/monaco-editor/monaco-editor.component';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [TasksListComponent, TasksCreatorComponent],
+  declarations: [TasksListComponent, TasksCreatorComponent, MonacoEditorComponent],
   exports: [TasksListComponent, TasksCreatorComponent],
-  imports: [CommonModule, CommonComponentsModule, TranslateModule],
+  imports: [CommonModule, CommonComponentsModule, TranslateModule, MatRadioModule],
 })
 export class TasksCreatorModule {}
--- frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator/tasks-creator.component.ts
+++ frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator/tasks-creator.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksTypes } from '../models/tasks-creator';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface ICreatedTask {
+  taskNameControl: string;
+  selectedTaskTypeControl: TasksTypes;
+  maxScoreControl: number;
+  mentorCheckControl: boolean;
+}
 
 @Component({
   selector: 'app-tasks-creator',
@@ -7,6 +15,9 @@ import { TasksTypes } from '../models/tasks-creator';
   styleUrls: ['./tasks-creator.component.scss'],
 })
 export class TasksCreatorComponent implements OnInit {
+  public isTaskTypesSelectorOpened: boolean;
+  public taskName: string;
+  public maxScore: number;
   public taskTypesEnum: typeof TasksTypes = TasksTypes;
   public isListEmpty: boolean = false;
   public tasksTypes: Set<TasksTypes> = new Set([
@@ -16,11 +27,40 @@ export class TasksCreatorComponent implements OnInit {
     TasksTypes.coding,
   ]);
   public selectedTaskType: TasksTypes = TasksTypes.single;
+  public isMentorCheckSelected: boolean;
+  public taskCreatorControl: FormGroup;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  public onOpenTaskTypesSelector(isOpen: boolean): void {
+    this.isTaskTypesSelectorOpened = isOpen;
+    if (this.selectedTaskType === TasksTypes.coding) {
+      this.isMentorCheckSelected = true;
+    }
+  }
+
+  public ngOnInit(): void {
+    this.taskCreatorControl = new FormGroup({
+      taskNameControl: new FormControl(),
+      selectedTaskTypeControl: new FormControl(),
+      maxScoreControl: new FormControl(),
+      mentorCheckControl: new FormControl(),
+    });
+
+    this.taskCreatorControl.valueChanges.subscribe(
+      ({ taskNameControl, selectedTaskTypeControl, maxScoreControl, mentorCheckControl }: ICreatedTask) => {
+        this.taskName = taskNameControl;
+        this.selectedTaskType = selectedTaskTypeControl;
+        this.maxScore = maxScoreControl;
+        this.isMentorCheckSelected = mentorCheckControl;
+      }
+    );
+
+  }
+
+  public isMentorCheckDisabled(): boolean {
+    return this.selectedTaskType === TasksTypes.coding;
   }
 
 }
