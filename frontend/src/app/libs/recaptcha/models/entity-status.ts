c89767556dad3f36b7f34eee20013bd969bac4d6
Tatsiana <Tatsiana_Shynkevich@epam.com>
2021-02-15 17:26:08 +0300

    feat: delete duplicate entities

--- frontend/src/app/libs/recaptcha/models/entity-status.ts
+++
@@ -1,6 +0,0 @@
-export enum EntityStatus {
-	Init = 'Init',
-	Pending = 'Pending',
-	Success = 'Success',
-	Error = 'Error'
-}
--- frontend/src/app/libs/recaptcha/models/entity-wraper.ts
+++
@@ -1,7 +0,0 @@
-import { EntityStatus } from './entity-status';
-
-export interface EntityWrapper<T> {
-	status: EntityStatus;
-	value?: T;
-	error?: any;
-}
