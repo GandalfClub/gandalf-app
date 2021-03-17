34ca39c7a44caa6e8eca13229205455d4b0f64bb
Maksim Protas <maksim_protas@epam.com>
2021-03-17 19:46:32 +0300

    fix: task creator unit tests

--- frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator/tasks-creator.component.spec.ts
+++ frontend/src/app/libs/common-components/components/tasks-creator/tasks-creator/tasks-creator.component.spec.ts
@@ -1,15 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
 
 import { TasksCreatorComponent } from './tasks-creator.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { Injector } from '@angular/core';
+import { of } from 'rxjs/internal/observable/of';
+import { FormBuilder } from '@angular/forms';
+
+const translations: any = { 'TASK-CREATION.LIST_HEADER_TITLE' : 'This is a test' };
+
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return of(translations);
+  }
+}
 
 describe('TasksCreatorComponent', () => {
   let component: TasksCreatorComponent;
   let fixture: ComponentFixture<TasksCreatorComponent>;
+  let mockTranslateService: TranslateService;
+  let injector: Injector;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: FakeLoader},
+        })
+      ],
       declarations: [TasksCreatorComponent],
+      providers: [FormBuilder],
     }).compileComponents();
+
+    injector = getTestBed();
+    mockTranslateService = injector.get(TranslateService);
+    mockTranslateService.use('en');
   }));
 
   beforeEach(() => {
