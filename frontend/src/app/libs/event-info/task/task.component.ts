706e82a9b876e9752a513a9a84683a9d73b0497b
Dmitry Bulavko <self-walking_cat@mail.ru>
2021-06-27 23:27:14 +0300

    task component: only visual, logic not completed

--- frontend/src/app/libs/event-info/task/task.component.ts
+++ frontend/src/app/libs/event-info/task/task.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { forkJoin, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { Task } from '../../landing/models/task.model';
+import { EntityWrapper } from '../../auth/models/entity-wraper';
+import { User } from '../../auth/models/user';
+import { ContainerFacadeService } from '../../container/services/container-facade.service';
+import { UserService } from '../../container/services/user.service';
+import { Solution, Task } from '../../landing/models/task.model';
 import { EventInfoFacadeService } from '../store/event-info-facade.service';
 
 @Component({
@@ -11,21 +16,65 @@ import { EventInfoFacadeService } from '../store/event-info-facade.service';
 })
 export class TaskComponent implements OnInit, OnDestroy {
 	public task: Task;
+	public solutions: Solution[];
+	public user: User;
+	public currentSolution: Solution;
+	public eventFormGroup: FormGroup;
 
 	private destroy$: Subject<void> = new Subject<void>();
 
-	constructor(private eventInfoFacadeService: EventInfoFacadeService) { }
+	constructor(
+		private formBuilder: FormBuilder,
+		private eventInfoFacadeService: EventInfoFacadeService,
+		private userService: ContainerFacadeService
+
+	) { }
 
 	public ngOnInit(): void {
+		this.eventFormGroup = this.formBuilder.group({
+			answer: ''
+		});
+
+		this.eventInfoFacadeService.fetchSolutions();
+
 		this.eventInfoFacadeService
 			.selectedTask$
 			.pipe(takeUntil(this.destroy$))
-			.subscribe((task: Task) => this.task = task);
+			.subscribe((task: Task) => {
+				this.task = task
+				if (this.task.taskType === 'CODING') {
+					this.eventFormGroup.value.answer = this.task.code
+				}
+
+				this.eventInfoFacadeService
+				.solutions$
+				.pipe(takeUntil(this.destroy$))
+				.subscribe((solutions: Solution[]) => {
+					this.solutions = solutions
+					this.currentSolution = this.solutions.filter((solution: Solution) => solution.taskId === this.task.id)[0]
+				});
+			});
+
+		this.userService
+			.user$
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((user: EntityWrapper<User>) => {
+				this.user = user.value
+			});
 
 	}
 
 	public ngOnDestroy(): void {
 		this.destroy$.next();
 		this.destroy$.complete();
+	}
+
+	public submitSolution(): void {
+		console.log(this.eventFormGroup.value)
+	}
+
+	public setCodeAnswer(data: string) {
+		if (this.task.taskType !== 'CODING') {return}
+		this.eventFormGroup.value.answer = data
 	}
 }
